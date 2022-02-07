dc9653e80436f34fc78bdec4430da63f66ae3055
Psami-wondah <okechukwusamuel16@gmail.com>
2022-02-07 14:07:11 +0100

    added resume

--- core/settings.py
+++ core/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
 import environ
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -144,4 +144,4 @@ MESSAGE_TAGS = {
         messages.ERROR: 'alert-danger',
  }
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
